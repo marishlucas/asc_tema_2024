eff8aaef56cd1732f315d6321b77ecda28af2c5d
marishlucas <lucas.m.maris@gmail.com>
2024-12-07 20:55:11 +0200

    feat: added the ability to ADD multiple files

--- main.s
+++ main.s
@@ -1,10 +1,14 @@
 .data 
   memory: .zero 1024
   formatAdd: .asciz "%d: (%d, %d)\n"
-  formatScanf: .asciz "%d: (%d, %d)\n"
+  formatScanf: .asciz "%ld"
   operations: .space 4
   descriptor: .space 4
   blocks: .space 4
+  nr_files: .space 4
+  size: .space 4
+  start_pos: .space 4
+  counter: .space 4
 
 .text
 .global main 
@@ -17,52 +21,78 @@ main:
   popl %ebx
 
   pushl $descriptor
-  pushl $formatScanf 
-  call scanf
-  popl %ebx
-  popl %ebx
-
-  pushl $blocks
   pushl $formatScanf
   call scanf
   popl %ebx
   popl %ebx
 
-  pushl blocks
+  pushl $nr_files
+  pushl $formatScanf  
+  call scanf
+  popl %ebx
+  popl %ebx
+
+  movl $0, start_pos
+  movl $0, counter
+
+add_files_loop:
+  movl counter, %ecx
+  cmpl nr_files, %ecx
+  jge exit 
+
+  pushl $descriptor
+  pushl $formatScanf
+  call scanf
+  popl %ebx
+  popl %ebx
+
+  pushl $size
+  pushl $formatScanf
+  call scanf
+  popl %ebx
+  popl %ebx
+
+  pushl size
   pushl descriptor
   call add_file
-  popl %ebx
-  popl %ebx
-  jmp exit
+  addl $8, %esp
+
+  incl counter
+  jmp add_files_loop
 
 add_file: 
   pushl %ebp
   movl %esp, %ebp
-  # la 4(%ebp) se afla adresa de ret
-  movl 8(%ebp), %edx
-  movl 12(%ebp), %ecx
-  movl $0, %edi
+  
+  movl 8(%ebp), %edx   # descriptor
+  movl 12(%ebp), %ecx  # size
+  movl start_pos, %edi
   lea memory, %esi
 
 storing_loop:
   cmpl %ecx, %edi
   jge storing_end
-  # descriptori de la 0-255 => putem folosi dl din edx
-  movb %dl, (%esi, %edi, 1)   #base + index * scale
+  movb %dl, (%esi, %edi, 1)
   incl %edi
   jmp storing_loop
   
 storing_end:
-  pushl %ecx 
-  decl (%esp)
-  pushl $0
+  movl start_pos, %edi
+  addl %ecx, %edi
+  decl %edi
+  
+  pushl %edi
+  pushl start_pos
   pushl %edx
   pushl $formatAdd
   call printf
+  addl $16, %esp
 
-  popl %ebx
-  popl %ebx
-  popl %ebx
+  incl %edi
+  movl %edi, start_pos
+  
+  pushl $0
+  call fflush
   popl %ebx
 
   movl %ebp, %esp

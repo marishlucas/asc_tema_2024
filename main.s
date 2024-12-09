1b3d37efe579fafd9a9c6e563a67aed45ddc8904
marishlucas <lucas.m.maris@gmail.com>
2024-12-09 21:15:54 +0200

    feat: added loop for multiple file adding

--- main.s
+++ main.s
@@ -1,11 +1,20 @@
+/*
+
+TODO: add remainder conversion for partial files
+
+  */
+
 .data 
   memory: .zero 1024
   formatAdd: .asciz "%d: (%d, %d)\n"
-  formatScanf: .asciz "%d: (%d, %d)\n"
+  formatScanf: .asciz "%ld"
   operations: .space 4
   descriptor: .space 4
   blocks: .space 4
   blockSize: .long 8
+  index: .long 0
+  nr_files: .space 4     
+  counter: .long 0       
 
 .text
 .global main 
@@ -17,8 +26,19 @@ main:
   popl %ebx
   popl %ebx
 
+  pushl $nr_files
+  pushl $formatScanf
+  call scanf
+  popl %ebx
+  popl %ebx
+
+add_files_loop:
+  movl counter, %eax
+  cmpl nr_files, %eax
+  jge exit
+
   pushl $descriptor
-  pushl $formatScanf 
+  pushl $formatScanf
   call scanf
   popl %ebx
   popl %ebx
@@ -32,42 +52,43 @@ main:
   movl blocks, %eax
   movl $0, %edx
   divl blockSize
-
   movl %eax, blocks
 
   pushl blocks
   pushl descriptor
   call add_file
-  popl %ebx
-  popl %ebx
-  jmp exit
+  addl $8, %esp
 
-add_file: 
+  incl counter
+  jmp add_files_loop
+
+add_file:
   pushl %ebp
   movl %esp, %ebp
   movl 8(%ebp), %edx
   movl 12(%ebp), %ecx
-  movl $0, %edi
+  movl index, %edi
   lea memory, %esi
 
 storing_loop:
-  cmpl %ecx, %edi
-  jge storing_end
   movb %dl, (%esi, %edi, 1)
   incl %edi
-  jmp storing_loop
-  
+  decl %ecx 
+  jnz storing_loop
+
 storing_end:
-  pushl %ecx 
-  pushl $0
+  movl index, %eax
+  decl %edi
+  
+  pushl %edi
+  pushl %eax
   pushl %edx
   pushl $formatAdd
   call printf
+  addl $16, %esp
 
-  popl %ebx
-  popl %ebx
-  popl %ebx
-  popl %ebx
+  incl %edi
+  movl %edi, index
 
   movl %ebp, %esp
   popl %ebp
